30cdd2331cf4100d3e743c6086e074e1d550834e
Petr <nikitin.p.94@gmail.com>
2020-11-07 20:21:38 +0300

    combine all functions in index file

---
+++ src/helpers/index.ts
@@ -0,0 +1,15 @@
+import createSasToken from "../helpers/createSasToken";
+import detectPlatform from "../helpers/detectPlatform";
+import distructMindboxAndswer from "../helpers/distructMindboxAndswer";
+import getMindboxInstallationInfo from "../helpers/getMindboxInstallationInfo";
+import sendCustomerToMindbox from "../helpers/sendCustomerToMindbox";
+import sendDataToAzure from "../helpers/sendDataToAzure";
+
+export {
+  createSasToken,
+  detectPlatform,
+  distructMindboxAndswer,
+  getMindboxInstallationInfo,
+  sendCustomerToMindbox,
+  sendDataToAzure,
+};
