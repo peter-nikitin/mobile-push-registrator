8f7c89a79a11bb527aea9d42080d72f210f1e293
Petr <nikitin.p.94@gmail.com>
2020-11-07 20:19:41 +0300

    add trycatch to api call

--- src/helpers/getMindboxInstallationInfo.ts
+++ src/helpers/getMindboxInstallationInfo.ts
@@ -1,20 +1,24 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { MbInstallationInfo } from "../declare/types.d";
 
 const getMindboxInstallationInfo = async (
-  endpoint: string
+  platfom: string
 ): Promise<AxiosResponse<MbInstallationInfo>> => {
-  const installationInfo = await axios.post<MbInstallationInfo>(
-    `https://api.mindbox.ru/v3/mobile-push/register?endpointId=${endpoint}`,
-    {},
-    {
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  );
+  try {
+    const installationInfo = await axios.post<MbInstallationInfo>(
+      `https://api.mindbox.ru/v3/mobile-push/register?endpointId=${process.env[platfom]}`,
+      null,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
 
-  return installationInfo;
+    return installationInfo;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default getMindboxInstallationInfo;
