7cb4de01954f88adca2f26c15182e92238e609aa
Petr <nikitin.p.94@gmail.com>
2020-11-10 17:58:33 +0300

    add intercaptors mock

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,4 +1,12 @@
 export default {
   get: jest.fn().mockRejectedValue(),
   post: jest.fn().mockRejectedValue(),
+  interceptors: {
+    request: {
+      use: jest.fn(),
+    },
+    response: {
+      use: jest.fn(),
+    },
+  },
 };
